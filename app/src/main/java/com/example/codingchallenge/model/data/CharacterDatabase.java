bccfefbdc300c111e66d133ebb99d13ea062e921
aasthaasher <aasthaasher24@gmail.com>
2023-08-31 13:26:33 -0700

    Implemented CharacterDatabase and CharacterRepository

--- app/src/main/java/com/example/codingchallenge/model/data/CharacterDatabase.java
+++ app/src/main/java/com/example/codingchallenge/model/data/CharacterDatabase.java
@@ -1,4 +1,33 @@
 package com.example.codingchallenge.model.data;
+import android.content.Context;
 
-public class CharacterDatabase {
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {CharacterEntity.class}, version = 1)
+public abstract class CharacterDatabase extends RoomDatabase {
+    public abstract CharacterDao characterDao();
+
+    public static volatile CharacterDatabase INSTANCE;
+    public static final int NUMBER_OF_THREADS = 4;
+    public static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    public static CharacterDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (CharacterDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    CharacterDatabase.class, "character-db")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
 }
--- app/src/main/java/com/example/codingchallenge/model/repository/CharacterRepository.java
+++ app/src/main/java/com/example/codingchallenge/model/repository/CharacterRepository.java
@@ -1,4 +1,72 @@
 package com.example.codingchallenge.model.repository;
 
+import android.app.Application;
+import android.os.AsyncTask;
+
+import com.example.codingchallenge.model.data.CharacterDao;
+import com.example.codingchallenge.model.data.CharacterDatabase;
+import com.example.codingchallenge.model.data.CharacterEntity;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 public class CharacterRepository {
+
+    public CharacterDao characterDao;
+    public CharacterDatabase characterDatabase;
+
+    public CharacterRepository(Application application) {
+        characterDatabase = CharacterDatabase.getDatabase(application);
+        characterDao = characterDatabase.characterDao();
+    }
+
+    public List<CharacterEntity> getAllCharacters() {
+        try {
+            return new GetCharsAsyncTask().execute().get();
+
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public void insert(CharacterEntity characterEntity) {
+        CharacterDatabase.databaseWriteExecutor.execute(() -> {
+            characterDao.insert(characterEntity);
+        });
+    }
+    public void delete(String name) {
+        CharacterDatabase.databaseWriteExecutor.execute(() -> {
+            characterDao.deleteContactById(name);
+        });
+    }
+
+    public boolean checkIfCharacterInDatabase(String name){
+        try {
+            return new checkIfExistsAsyncTask().execute(name).get();
+
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public class GetCharsAsyncTask extends AsyncTask<Void, Void, List<CharacterEntity>>
+    {
+        @Override
+        protected List<CharacterEntity> doInBackground(Void... url) {
+            return characterDao.getAllCharacters();
+        }
+    }
+
+    public class checkIfExistsAsyncTask extends AsyncTask<String, Void, Boolean>
+    {
+        @Override
+        protected Boolean doInBackground(String... name) {
+            String charName = name[0];
+            return characterDao.getCharacterByName(charName)!=null;
+        }
+    }
 }
