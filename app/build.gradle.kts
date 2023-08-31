2ad995a37eeaf68b902da44ea9df201bd4b26979
aasthaasher <aasthaasher24@gmail.com>
2023-08-31 13:11:17 -0700

    Added dependencies for ROOM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.squareup.retrofit2:retrofit:2.9.0") // Retrofit library dependency
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")  // Retrofit library dependency
+    // Room dependency
+    implementation("androidx.room:room-runtime:2.5.2")
+    annotationProcessor("androidx.room:room-compiler:2.5.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
