b9ecdafc2d1a33be6589e45bb9acfaaae9005762
aasthaasher <aasthaasher24@gmail.com>
2023-08-31 13:46:00 -0700

    Implemented CharacterViewModel

--- app/src/main/java/com/example/codingchallenge/viewModel/CharacterViewModel.java
+++ app/src/main/java/com/example/codingchallenge/viewModel/CharacterViewModel.java
@@ -1,4 +1,30 @@
 package com.example.codingchallenge.viewModel;
 
-public class CharacterViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+
+import com.example.codingchallenge.model.repository.CharacterRepository;
+import com.example.codingchallenge.model.data.CharacterEntity;
+
+import java.util.List;
+
+public class CharacterViewModel extends AndroidViewModel {
+    private CharacterRepository characterRepository;
+
+    public CharacterViewModel (Application application) {
+        super(application);
+        characterRepository = new CharacterRepository(application);
+    }
+    public List<CharacterEntity> getAllCharacters() {
+        return characterRepository.getAllCharacters();
+    }
+
+    public boolean checkIfCharacterInDatabase(String name)
+    {
+        return characterRepository.checkIfCharacterInDatabase(name);
+    }
+    public void delete(String name) { characterRepository.delete(name); }
+
+    public void insert(CharacterEntity characterEntity) { characterRepository.insert(characterEntity); }
 }
