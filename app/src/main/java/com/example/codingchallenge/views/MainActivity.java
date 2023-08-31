a5f9ce0c6174fc81ce4d9fcf086c5a1559150648
aasthaasher <aasthaasher24@gmail.com>
2023-08-31 15:02:40 -0700

    Added method to access saved characters in Main Activity

--- app/src/main/java/com/example/codingchallenge/views/MainActivity.java
+++ app/src/main/java/com/example/codingchallenge/views/MainActivity.java
@@ -1,23 +1,31 @@
 package com.example.codingchallenge.views;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.codingchallenge.R;
+import com.example.codingchallenge.model.data.CharacterEntity;
+import com.example.codingchallenge.viewModel.CharacterViewModel;
 import com.example.codingchallenge.views.adapters.CharacterAdapter;
 import com.example.codingchallenge.model.network.SwapidevService;
 import com.example.codingchallenge.model.data.Character;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import com.example.codingchallenge.model.network.CharacterResponse;
@@ -37,6 +45,11 @@ public class MainActivity extends AppCompatActivity implements CharacterAdapter.
     // Reference to the ongoing API call
     private Call<CharacterResponse> currentApiCall;
     SharedPreferences sharedpreferences;
+    public List<CharacterEntity> allCharsInDb;
+    public List<String> charsInDB;
+    CharacterViewModel characterViewModel;
+    Button showDatabaseButton;
+    String charsInDBMessage;
 
     public static final String fav_list = "favorites_list";
     Set<String> favoritesList;
@@ -47,6 +60,9 @@ public class MainActivity extends AppCompatActivity implements CharacterAdapter.
 
         // Retrieve the favorites list when the activity is restarted
         favoritesList = sharedpreferences.getStringSet(fav_list, null);
+
+        // Get saved characters
+        getDatabaseCharacters();
 
         // Reset the adapter to ensure the favorites are loaded correctly
         characterAdapter = new CharacterAdapter(characterList, favoritesList, this);
@@ -61,11 +77,34 @@ public class MainActivity extends AppCompatActivity implements CharacterAdapter.
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getDatabaseCharacters();
 
         initializeComponents();
 
         // Set up search functionality
         setUpSearchView();
+
+        showDatabaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(charsInDBMessage);
+                if(Objects.equals(charsInDBMessage, "")){
+                    builder.setMessage("No saved characters");
+                }
+                else {
+                    builder.setMessage(charsInDBMessage);
+                }
+                builder.setTitle("Characters in Database")
+                        .setPositiveButton("Done", (DialogInterface.OnClickListener) (dialog, which) -> {
+                    dialog.cancel();
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
 
     }
 
@@ -78,6 +117,8 @@ public class MainActivity extends AppCompatActivity implements CharacterAdapter.
         // Initialize UI components
         searchView = findViewById(R.id.searchView);
         recyclerView = findViewById(R.id.recyclerView);
+        showDatabaseButton = findViewById(R.id.showDatabaseButton);
+
 
         // Initialize RecyclerView and adapter
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -95,6 +136,19 @@ public class MainActivity extends AppCompatActivity implements CharacterAdapter.
         recyclerView.setAdapter(characterAdapter);
 
     }
+
+    private void getDatabaseCharacters() {
+        characterViewModel = new ViewModelProvider(this).get(CharacterViewModel.class);
+        allCharsInDb = characterViewModel.getAllCharacters();
+        charsInDBMessage = "";
+        charsInDB = new ArrayList<>();
+
+        for (CharacterEntity characterEntity : allCharsInDb) {
+            charsInDB.add(characterEntity.getName());
+            charsInDBMessage += characterEntity.getName() + "\n";
+        }
+    }
+
     private void setUpSearchView() {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
