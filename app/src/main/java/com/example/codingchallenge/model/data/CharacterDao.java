052088f04439c5c019e6b8ffbdafecfda7b95bb4
aasthaasher <aasthaasher24@gmail.com>
2023-08-31 13:23:28 -0700

    Implemented CharacterDao and CharacterEntity

--- app/src/main/java/com/example/codingchallenge/model/data/CharacterDao.java
+++ app/src/main/java/com/example/codingchallenge/model/data/CharacterDao.java
@@ -1,4 +1,26 @@
 package com.example.codingchallenge.model.data;
 
-public class CharacterDao {
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.codingchallenge.model.data.CharacterEntity;
+
+import java.util.List;
+
+@Dao
+public interface CharacterDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(CharacterEntity character);
+
+    @Query("DELETE FROM characters WHERE name = :characterName")
+    void deleteContactById(String characterName);
+
+    @Query("SELECT * FROM characters")
+    List<CharacterEntity> getAllCharacters();
+
+    @Query("SELECT * FROM characters WHERE name = :characterName")
+    CharacterEntity getCharacterByName(String characterName);
+
 }
--- app/src/main/java/com/example/codingchallenge/model/data/CharacterEntity.java
+++ app/src/main/java/com/example/codingchallenge/model/data/CharacterEntity.java
@@ -1,4 +1,97 @@
 package com.example.codingchallenge.model.data;
 
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "characters")
 public class CharacterEntity {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public String name;
+    public String hairColor;
+    public String skinColor;
+    public String eyeColor;
+    public String birthYear;
+    public String gender;
+    public String height;
+    public String mass;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public void setMass(String mass) {
+        this.mass = mass;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public void setSkinColor(String skinColor) {
+        this.skinColor = skinColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public void setBirthYear(String birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public String getMass() {
+        return mass;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public String getSkinColor() {
+        return skinColor;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+
+
 }
+
